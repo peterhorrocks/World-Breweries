977ad6cdd654ddaae5e76c5ebb42d7f7f223b538
peterhorrocks <107364961+peterhorrocks@users.noreply.github.com>
2025-02-05 01:36:30 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# World-Breweries
-World Breweries API Project
+# World Breweries API Project #
+
+This is my contirbution to the UDEMY course project, **The Complete Full Stack Web Development Bootcamp, Capstone Project - Use a Public API**.
+
+This is also my first attempt at a README.md file using Markdown language, so forgive me if I get a few things wrong.
+
+My four passions in life are programming, beer, hiking and biology/zoology. This project covers the first 2. I intend to tackle the others in upcoming projects.
+
+This project uses the **Open Brewery DB API**, and provides you with various options to view the data.
+
+You can view the documentation and required /endpoints at <a href="https://www.openbrewerydb.org/">**Open Brewery DB**</a>
+
+## Download ##
+
+You are welcome to download and install on you computer, and amend the code as you wish.
+
+Extract the zip file, and copy the extracted folder under an **empty project** folder of your choice.
+
+## Installing Node ##
+
+This is a **server-side** application, and you will have needed to installed **Node.js** on your computer.
+
+If you do not already have **Node.js** installed, see <a href="https://nodejs.org/en">**Node JS**</a> for instructions.
+
+## Installing the required Node Modules ##
+
+This project uses the **axios**, **express**, **ejs** and **body-parser** node modules, so these will need to be installed.
+
+Open a **terminal** in your Code Editor; I am using VS Code, but most IDEs should be similar.
+
+In the **command line**, make sure that you are in your project directory.
+
+1.  Run **npm init** to initialise and create the **package.json** file.
+
+2.  Edit the **package.json** file, and include the entry **"type": "module",** under the "description". Save the file.
+
+3.  Next, run **npm install axios** or **npm i axios**. This will create a folder **node_modules^^ in your project folder, and install the axios module.
+
+4.  Do this for the **express**, **ejs** and **body-parser** node modules. These will automatically be included in the packag.json file under **"dependencies".**
+
+## Running the Package ##
+
+1.  From the **command line**, run **node index.js**, or **nodemon index.js** if you have nodemon installed.
+
+2.  In your **browser**, type in the address bar **localhost:3000**.
+
+3.  And away we go!
+
+## Contributions ##
+
+Anyone is welcome to contribute to this project and give me new ideas.
+
+
+#### Author: Peter Horrocks ####
+
+
+
---
+++ index.js
@@ -0,0 +1,166 @@
+// Import the required modules
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+let apiURL = "";
+
+// Set the public folder for static files
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Home Page launch route
+// Get a random entry when displaying the home page.
+app.get("/", async (req, res) => {
+    console.log("GET Home Page")
+    
+    try {
+
+        /* Request data from API */
+        const response = await axios.get(
+            "https://api.openbrewerydb.org/v1/breweries/random");
+        const result = response.data;
+        console.log(result);
+
+        // Send API data to Home page
+        res.render("index.ejs", {
+            breweryData: result,
+        });
+
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+
+});
+
+
+// Get details for a "Particular Brewery"
+app.post("/name", async (req, res) => {
+    console.log("GET /name")
+
+    /* Receive data from HTML form */
+    let name = req.body.name;
+
+    try {
+        /* Request data from API */
+        const response = await axios.get(
+            `https://api.openbrewerydb.org/v1/breweries?by_name=${name}`);
+        const result = response.data;
+
+        // Send API data to Home page
+        res.render("index.ejs", {
+            breweryData: result,
+        })
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+ 
+});
+
+
+// Get the brewery by the "Type"
+app.post("/type", async (req, res) => {
+    console.log("GET /type")
+
+    /* Receive data from HTML form */
+    let type = req.body.type;
+
+    try {
+        /* Request data from API */
+        const response = await axios.get(
+            `https://api.openbrewerydb.org/v1/breweries?by_type=${type}`);
+        const result = response.data;
+
+        // Send API data to Home page
+        res.render("index.ejs", {
+            breweryData: result,
+        })
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        let errorMessage = "Please select a Type"
+        res.render("index.ejs", {
+        error: errorMessage,
+      });
+    }
+ 
+});
+
+
+// Get the brewery by the "County" or "US State"
+app.post("/county", async (req, res) => {
+    console.log("GET /county")
+
+    /* Receive data from HTML form */
+    let county = req.body.county;
+
+    if (county === "") {
+        apiURL = `https://api.openbrewerydb.org/v1/breweries?by_state=${county}&sort=county`;
+    } else {
+        apiURL = `https://api.openbrewerydb.org/v1/breweries?by_state=${county}`;
+    }
+
+    try {
+        /* Request data from API */
+        const response = await axios.get(apiURL);
+        const result = response.data;
+
+        // Send API data to Home page
+        res.render("index.ejs", {
+            breweryData: result,
+        })
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+ 
+});
+
+
+// Get the brewery by the "Country"
+app.post("/country", async (req, res) => {
+    console.log("GET /country")
+
+    /* Receive data from HTML form */
+    let country = req.body.country;
+
+    if (country === "") {
+        apiURL = `https://api.openbrewerydb.org/v1/breweries?by_country=${country}&sort=country`;
+    } else {
+        apiURL = `https://api.openbrewerydb.org/v1/breweries?by_country=${country}`;
+    }
+
+    try {
+    /* Request data from API */
+    const response = await axios.get(apiURL);
+        const result = response.data;
+
+        // Send API data to Home page
+        res.render("index.ejs", {
+            breweryData: result,
+        })
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+ 
+});
+
+
+// Server listening for events on Port 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
